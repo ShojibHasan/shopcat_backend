c8f8a90d32eadbc6be5b1ddda44ca982d154a693
shojibhasan <shojibhasan15@gmail.com>
2021-02-18 23:01:15 +0600

    Home page html design

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('api',views.getRoutes,name='routes'),
+    path('',views.home,name='home'),
 ]
--- shopcat_backend/settings.py
+++ shopcat_backend/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'shopcat_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,10 +149,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'shopcat_backend/static')
+]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
