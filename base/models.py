b0b67ff3a7ddec4444d8b24f796ff33e89b44c12
shojibhasan <shojibhasan15@gmail.com>
2021-02-19 21:39:40 +0600

    pillow installed

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ from django.db.models.fields import AutoField
 class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200,null=True,blank=True)
-    # image =
+    image = models.ImageField(,null=True,blank=True)
     brand = models.CharField(max_length=200,null=True,blank=True)
     category = models.CharField(max_length=200,null=True, blank=True)
     description = models.TextField(null=True,blank=True)
