247d178f14cde3580a882626a022e0629288a8b2
shojibhasan <shojibhasan15@gmail.com>
2021-02-21 01:18:28 +0600

    product api create

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .import views
+
+from . import views
 
 urlpatterns = [
     path('api',views.getRoutes,name='routes'),
+    path('api/product/all',views.getProducts,name='products'),
+    path('api/product/<int:pk>',views.getProduct,name='product'),
     path('',views.home,name='home'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,8 @@ from django.http import JsonResponse, response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import *
+from .serializers import *
 # Create your views here.
 
 
@@ -10,5 +11,20 @@ def getRoutes(request):
     return JsonResponse('Hello',safe=False)
 
 
+
 def home(request):
     return render(request,'index.html')
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
