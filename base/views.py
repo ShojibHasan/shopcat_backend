047ee5751a8397b65a17a3dc14516df6e871fd61
shojibhasan <shojibhasan15@gmail.com>
2021-03-26 23:47:48 +0600

    jwt token modified with username and email

--- base/views.py
+++ base/views.py
@@ -1,10 +1,26 @@
+from django.contrib.auth.models import update_last_login
 from django.http import JsonResponse, response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+       
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 def getRoutes(request):
